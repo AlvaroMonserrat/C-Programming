7087e469b31c1c4317b44c47e54f199e46738b14
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2018-11-30 10:34:23 -0300

    ok

--- BasicFunction/BasicFunction/main.c
+++ BasicFunction/BasicFunction/main.c
@@ -1,11 +1,11 @@
+/*--------------------------------------------------------------------------------
+	Dartmouth_IMTx: DART.IMT.C.03
+	C Programming: Modular Programming and Memory Management
+	Unit 3.1: Functions and recursion
+---------------------------------------------------------------------------------*/
 #include<stdio.h>
 
-
-int sum(int x, int y) {
-
-	return x+y;
-}
-
+int sum(int x, int y);
 
 int main() {
 
@@ -14,6 +14,7 @@ int main() {
 
 	printf("Ingrese un numero: \n");
 	scanf_s("%d", &a);
+
 	printf("Ingrese otro numero: \n");
 	scanf_s("%d", &b);
 
@@ -22,9 +23,11 @@ int main() {
 	result = sum(a, b);
 	printf("La suma es: %d\n", result);
 	
-
 	system("pause");
 
-
 	return 0;
+}
+
+int sum(int x, int y) {
+	return x + y;
 }
