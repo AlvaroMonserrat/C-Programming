2e6503bcd9ae3d1d5657a5c23005403bd7241951
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2018-11-30 15:32:35 -0300

    ok

--- BasicFunction/BasicFunction/main.c
+++ BasicFunction/BasicFunction/main.c
@@ -7,11 +7,11 @@
 
 #define MAX_ARRAYSIZE (50)
 
-void metricFunction(int n, float *a, char *b);
+void metricFunction(int n, double *a, char *b);
 
 int main(void) {
 
-	float inputData[MAX_ARRAYSIZE];
+	double inputData[MAX_ARRAYSIZE];
 	char inputDataType[MAX_ARRAYSIZE];
 	int numberOfData;
 
@@ -26,27 +26,44 @@ int main(void) {
 }
 
 
-void metricFunction(int n, float *a, char *b) {
+void metricFunction(int n, double *a, char *b) {
 
 	for (int i = 0;i < n;i++) {
-		scanf_s("%f %c", &a[i], &b[i]);
+		scanf_s("%lf %c", &a[i], &b[i]);
 		if (b[i] == 'm') {
 			a[i] = (a[i] / 0.3048);
+			
 		}
 		else if (b[i] == 'g')
 		{
 			a[i] = (a[i] * 0.002205);
+		
 		}
 		else if (b[i] == 'c')
 		{
 			a[i] = (a[i]*1.8+ 32 );
+	
 		}
 	}
 
 	printf("\n");
 
 	for (int i = 0;i < n;i++) {
-		printf("%.6f %c\n", a[i], b[i]);
+		printf("%.6lf ", a[i]);
+		
+		if (b[i] == 'm') {
+			printf("ft\n");
+		}
+		else if (b[i] == 'g')
+		{
+			printf("lbs\n");
+		}
+		else if (b[i] == 'c')
+		{
+			printf("f\n");
+		}
+		
+
 	}
 }
 
