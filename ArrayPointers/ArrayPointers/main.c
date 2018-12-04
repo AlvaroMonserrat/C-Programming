792932a28d2ad84324c4f4dddaaed94df88201a3
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2018-12-04 17:17:10 -0300

    Update

---
+++ ArrayPointers/ArrayPointers/main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void swapArray(int *);
+
+int main() {
+
+	int arrayOfsix[6];
+
+	for (int i = 0; i < 6;i++)
+	{
+		scanf_s("%d", &arrayOfsix[i]);
+	}
+
+	//swap data
+	swapArray(arrayOfsix);
+
+	for (int i = 0; i < 6;i++)
+	{
+		printf("%d ", arrayOfsix[i]);
+
+	}
+
+	system("pause");
+	return 0;
+}
+
+void swapArray(int *ptr) {
+
+	int temp;
+	int x = 0;
+	int y = 5;
+
+	while(x < y){
+		temp = *(ptr + x);
+		*(ptr + x) = *(ptr + y);
+		*(ptr + y) = temp;
+		x++;
+		y--;
+	}
+}
