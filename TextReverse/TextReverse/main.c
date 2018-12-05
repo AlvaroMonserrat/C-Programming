78b5708fad7e7e4bf76e2051fd369a8d343e94a6
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2018-12-05 18:58:18 -0300

    4th Course

---
+++ TextReverse/TextReverse/main.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//Texto:
+/*Science Computer on Papers Selected Knuth, Ervin Donald ― correct." be will results the that reader a 
+convince to and works algorithm an way the communicate to concepts, mathematical as well as forms literary 
+and aesthetic traditional with works who essayist an ideally is programmer A clearly. them understand can 
+beings human that so and quickly them perform can machines computing that so written are programs best "The*/
+
+int main() {
+	
+	char words[68][15];  // 68 palabras de 15 caracteres cada una
+
+	int i = 0;
+
+	//Escribir Texto
+	for (i = 0; i < 68; i++) {
+		scanf("%s", words[i]);
+	}
+	printf("\n");
+
+	//Imprimir Texto
+	for (i = 0; i < 68; i++) {
+		printf("%s ", words[67-i]);
+	}
+
+	printf("\n");
+
+	system("pause");
+
+	return 0;
+}
