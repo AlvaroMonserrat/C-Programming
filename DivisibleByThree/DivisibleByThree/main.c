d8db3911b2e3d2d8c5e952111f97e740de7c1f9d
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2018-12-13 16:39:48 -0300

    5th

--- DivisibleByThree/DivisibleByThree/main.c
+++ DivisibleByThree/DivisibleByThree/main.c
@@ -16,6 +16,10 @@ struct digit *readNumber(void);
 struct digit * append(struct digit * end, struct digit * newDigptr);
 struct digit *createDigit(int dig);
 int changeThrees(struct digit *);
+int countRedun(struct digit *);
+void putDigit(struct digit *, int);
+int isExistDig(struct digit *start, int digit);
+int howManyTime(struct digit *start, int digit);
 
 int main(void) {
 	struct digit *start;
@@ -23,10 +27,9 @@ int main(void) {
 
 	printf("The number ");
 	printNumber(start);
-	printf("was modified in %d places.\n", changeThrees(start));
 
-	printf("The new number is ");
-	printNumber(start);
+	printf("contains %d redundancies.\n", countRedun(start));
+
 	freeNumber(start);
 	system("pause");
 	return 0;
@@ -112,4 +115,80 @@ int changeThrees(struct digit *start) {
 		ptr = ptr->next;
 	}
 	return countDigit;
+}
+
+void putDigit(struct digit *start, int newdig) {
+	struct digit *ptr = start;
+	struct digit *newptr = NULL;
+
+	newptr = createDigit(newdig);
+
+	while (ptr->next != NULL) {
+		ptr = ptr->next; //Avanzar al final de la lista existente
+	}
+	ptr->next = newptr; //Enlazar nuevo digito con la lista
+}
+
+//si existe el digito retorna un 1;
+int isExistDig(struct digit *start, int digit) {
+	
+	struct digit *ptr = start;
+	int checkDigit = 0;
+	while (ptr != NULL) {
+		if (ptr->num == digit) {
+			checkDigit = 1;
+		}
+		ptr = ptr->next; //Avanzar al final de la lista existente
+	}
+	return checkDigit;
+}
+
+int howManyTime(struct digit *start, int digit) {
+	struct digit *ptr = start;
+	int countDigit = 0;
+
+	while (ptr != NULL) {
+		if (ptr->num == digit) {
+			countDigit++;
+		}
+		ptr = ptr->next;
+	}
+	return countDigit;
+}
+
+int countRedun(struct digit *start) {
+
+	struct digit *ptr = start;
+	struct digit *nstart = NULL; // new linked list
+	struct digit *nptr = NULL;
+	
+	int countTotal = 0;
+	int countTime = 0;
+
+	if (ptr != NULL) {
+		nstart = createDigit(ptr->num);
+		ptr = ptr->next;
+	}
+	
+	while (ptr != NULL)
+	{
+		if (!isExistDig(nstart, ptr->num)) {
+			putDigit(nstart, ptr->num);
+		}
+		ptr = ptr->next;
+	}
+	
+	nptr = nstart;
+
+	while (nptr != NULL) {
+
+		countTime = howManyTime(start, nptr->num);
+		if (countTime) {
+			countTotal += (countTime - 1);
+		}
+		nptr = nptr->next;
+	}
+
+	freeNumber(nstart);
+	return countTotal;
 }
